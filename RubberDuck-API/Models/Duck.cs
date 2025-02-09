677273100decb48ecef5ec9738c603cb71d38587
BeeBarry <barry.namdari@gmail.com>
2025-02-09 17:16:11 +0100

    Created Duck class

---
+++ RubberDuck-API/Models/Duck.cs
@@ -0,0 +1,11 @@
+namespace RubberDuck_API.Models;
+
+public class Duck
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    public string Speciality { get; set; }
+    public string Personality { get; set; }
+    public string Motto { get; set; }
+}
