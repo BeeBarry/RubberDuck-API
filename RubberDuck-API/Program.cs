9b13e06f11e70ee0ef142b7c6bf1f0ee3c995029
BeeBarry <barry.namdari@gmail.com>
2025-02-11 16:38:48 +0100

    Added CRUD functionality

--- RubberDuck-API/Program.cs
+++ RubberDuck-API/Program.cs
@@ -22,7 +22,62 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet()
+//READ
+app.MapGet("/ducks", async (DuckDbContext db, int id) =>
+{
+    return await db.Ducks.ToListAsync();
+});
+
+// READ by ID
+app.MapGet("/ducks/{id}", async ( DuckDbContext db,int id) =>
+{
+    var duck = await db.Ducks.FirstOrDefaultAsync(d => d.Id == id);
+
+    if (duck == null)
+        return Results.NotFound();
+
+    return Results.Ok(duck);
+});
+
+// CREATE
+app.MapPost("/ducks", async (Duck duck, DuckDbContext db) => 
+{
+    
+      db.Ducks.Add(duck);
+    await db.SaveChangesAsync();
+        return Results.Created($"/ducks/{duck.Id}", duck);
+            });
+
+// UPDATE 
+app.MapPut("/ducks/{id}", async (DuckDbContext db, int id, Duck updatedDuck) =>
+{
+    var duck = await db.Ducks.FirstOrDefaultAsync(d => d.Id == id);
+    if (duck == null)
+        return Results.NotFound();
+
+    duck.Name = updatedDuck.Name;
+    duck.Specialty = updatedDuck.Specialty;
+    duck.Personality = updatedDuck.Personality;
+    duck.Motto = updatedDuck.Motto;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+//DELETE
+app.MapDelete("/ducks/{id}", async (DuckDbContext db, int id) =>
+{
+    var duck = await db.Ducks.FirstOrDefaultAsync(d => d.Id == id);
+    if (duck == null)
+        return Results.NotFound();
+
+    db.Ducks.Remove(duck);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+
+
 
 
 app.Run();
