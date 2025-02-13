361b3a2d8a6987618e48c687bad3f3c6d2d8cb8d
BeeBarry <barry.namdari@gmail.com>
2025-02-13 15:41:04 +0100

    fix: express to use '.' instead of public since we go without public folder

--- duck-express-api/app.js
+++ duck-express-api/app.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('.'));
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname+ '/index.html');
