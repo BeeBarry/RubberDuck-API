30a0a72bfde2e9a2e03dadb361c0cb0c63924040
BeeBarry <barry.namdari@gmail.com>
2025-02-13 10:39:32 +0100

    Get- and Post endpoints added got fetch and post from .Net API

--- duck-express-api/app.js
+++ duck-express-api/app.js
@@ -1,23 +1,50 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.get('/', (req,res) => {
     res.sendFile(__dirname+ '/index.html');
-})
-
-app.get('/api/test', (req, res) => {
-    res.json({message: 'Hello from Express!'});
-})
-
-app.get('/api/ducks', async (req, res) => {
-    //Koden för att hämta från .NET api
 });
 
-app.post('/api/ducks', (req, res) => {
-   //Koden för att posta data till NET api 
+
+
+app.get('/api/ducks', async (req, res) => {
+    try {
+        const response = await fetch(`${process.env.API_URL}/ducks`);
+        const ducks = await response.json();
+        res.json(ducks);
+    } catch (error) {
+        console.error('Error when fetching ducks:', error);
+        res.status(500).json({error: 'Cant fetch ducks from .NET API'});
+    }
+    
+});
+
+app.post('/api/ducks', async (req, res) => {
+    try {
+        const response = await fetch(`${process.env.API_URL}/ducks`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(req.body)
+        });
+        
+        if(!response.ok) {
+            throw new Error('Error when creating new duck');
+        }
+        
+        const newDuck = await response.json();
+        res.status(201).json(newDuck);
+    } catch (error) {
+        console.error('Fel vid skapande av anka', error);
+        res.status(500).json({error: 'Could not create duck in .Net API'});
+    }
 });
 
 app.listen(port, () => {
